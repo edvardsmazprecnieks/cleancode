2cd8b743060d801671bc9deab24e624ca313f807
arnestrickmann <115920878+arnestrickmann@users.noreply.github.com>
2023-03-27 10:05:43 +0200

    changes - clean up

--- test_git.py
+++ test_git.py
@@ -2,8 +2,11 @@ import unittest
 import os
 from git import do_command
 
+"""Changed the comments into doc strings and made them more concise"""
+"""Function and variable names should be descriptive and follow the naming conventions, so I changed them"""
 
-def test_do_command_commit_invalid_file_path():
+
+def test_commit_with_invalid_file_paths():
     """Test do_command function to commit invalid file paths"""
     filePaths = ["invalid_path1.txt", "invalid_path2.txt"]
     message = "test commit message"
@@ -15,7 +18,7 @@ def test_do_command_commit_invalid_file_path():
         assert os.path.exists(filePaths[1]) == False
 
 
-def test_do_command_commit_no_message():
+def test_commit_with_no_message():
     filePaths = ["tests/test_file1.txt", "tests/test_file2.txt"]
     """Test do_command function to commit with no message"""
     message = ""
@@ -23,21 +26,21 @@ def test_do_command_commit_no_message():
     assert result == "Please enter a commit message"
 
 
-def test_do_command_diff_invalid_file_path():
+def test_diff_with_invalid_file_paths():
     """Test do_command function for diffing invalid file paths"""
     versions = ["invalid_path1.txt", "invalid_path2.txt"]
     result = do_command("diff", [], [], [], versions, "")
     assert result == "file is not a valid file path"
 
 
-def test_do_command_diff_files_identical():
+def test_diff_files_identical():
     """Test do_command to diffing files that are identical"""
     versions = ["test_file1.txt", "test_file1_copy.txt"]
     result = do_command("diff", [], [], [], versions, "")
     assert result == "Files are identical"
 
 
-def test_do_command_diff_files_different():
+def test_diff_files_different():
     """Test do_command function for diffing different files"""
     versions = ["test_file1.txt", "test_file2.txt"]
     result = do_command("diff", [], [], [], versions, "")
